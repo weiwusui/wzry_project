82e458566acd7e76249b2266d7ebe1ad2ae7bee2
weiwusui <1668695342@qq.com>
2022-08-18 14:26:29 +0800

    mongoose数据库连接

--- server/plugins/db.js
+++ server/plugins/db.js
@@ -1,6 +1,5 @@
 module.exports = app => {
     const mongoose = require('mongoose')
-    // const mysql = require('mysql')
     mongoose.connect('mongodb://127.0.0.1:27017/node-vue-moba',{
         useNewUrlParser:true,useUnifiedTopology: true 
     })
