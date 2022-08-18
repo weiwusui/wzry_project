29456cf3eb7870aff9ba83a5cbd4af33deecda61
weiwusui <1668695342@qq.com>
2022-08-18 14:03:53 +0800

    物品编辑界面

--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="about">
     <h1>{{id ? '编辑' : '新建'}}物品</h1>
-  
   <el-form label-width="120px" @submit.native.prevent="save">
-   
     <el-form-item label="名称">
       <el-input v-model="model.name"></el-input>
     </el-form-item>
@@ -20,7 +18,6 @@
       </el-upload>
     </el-form-item>
     <el-form-item>
-      
       <el-button type="primary" native-type="submit">
         保存
       </el-button>
@@ -32,13 +29,11 @@
   export default{
     props:{
       id:{
-
       }
     },
     data(){
       return {
         model:{
-          
         },
       }
     },
@@ -53,7 +48,6 @@
         else{
           res = await this.$http.post('rest/items',this.model)
         }
-        
         this.$router.push('/items/list')
         this.$message({
           type:'success',
@@ -64,10 +58,8 @@
         const res = await this.$http.get(`rest/items/${this.id}`)
         this.model = res.data
       },
-      
     },
     created(){
-      
       this.id&&this.fetch()
     }
   }
