d0942d6809854845c8a93d157bfb88ed11dab8ef
weiwusui <1668695342@qq.com>
2022-08-25 17:42:30 +0800

    修改了fixuploadurl上传文件地址

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -50,7 +50,8 @@ module.exports = app => {
     const upload = multer({dest : __dirname + `/../../uploads`})
     app.post('/admin/api/upload',authMiddleware(), upload.single('file'), async (req,res) =>{
       const file =req.file
-      file.url=`http://localhost:3000/uploads/${file.filename}`
+      // file.url=`http://localhost:3000/uploads/${file.filename}`
+      file.url=`http://139.224.18.36/uploads/${file.filename}`
       res.send(file)
     }),
     app.post('/admin/api/login',async(req,res)=>{
